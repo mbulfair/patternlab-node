58fc4f0e0df141448964e9db3296b2e776d10c80
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2017-05-24 10:46:30 -0500

    failing unit test
    part of https://github.com/pattern-lab/patternlab-node/issues/457

--- test/parent_list_item_hunter_tests.js
+++ test/parent_list_item_hunter_tests.js
@@ -2,12 +2,15 @@
 
 var tap = require('tap');
 
+var fs = require('fs-extra');
 var lih = require('../core/lib/list_item_hunter');
 var Pattern = require('../core/lib/object_factory').Pattern;
 var PatternGraph = require('../core/lib/pattern_graph').PatternGraph;
 var extend = require('util')._extend;
 var pa = require('../core/lib/pattern_assembler');
+var ph = require('../core/lib/parameter_hunter');
 var pattern_assembler = new pa();
+var parameter_hunter = new ph();
 
 // fake pattern creators
 function createFakeListPattern(customProps) {
@@ -81,58 +84,57 @@ function createFakePatternLab(customProps) {
 }
 
 
-tap.test('process_list_item_partials - correctly ignores bookended partials without a style modifier when the same partial has a style modifier between', function(test){
+tap.test('process_list_item_partials - renders listItems when pattern included with patternparameter', function(test){
   //arrange
-  var fs = require('fs-extra');
-  var pa = require('../core/lib/pattern_assembler');
-  var pattern_assembler = new pa();
   var list_item_hunter = new lih();
   var patterns_dir = './test/files/_patterns';
-  
+
   var pl = {};
   pl.config = {};
-  pl.data = {};
+  pl.data = { title: "test"};
   pl.data.link = {};
   pl.config.debug = false;
   pl.patterns = [];
   pl.partials = {};
   pl.config.patterns = { source: patterns_dir };
   pl.listitems = {
-    "1": [
-      {
-        "message": "Foo"
+    "1": {
+      "headline": {
+        "short": "a"
       }
-    ],
-    "2": [
-      {
-        "message": "Foo"
-      },
-      {
-        "message": "Bar"
+    },
+    "2": {
+      "headline": {
+        "short": "b"
       }
-    ]
+    },
+    "3": {
+      "headline": {
+        "short": "c"
+      }
+    }
   };
-  
+
   var listPattern = new Pattern('00-test/457-listitem.mustache');
   listPattern.template = fs.readFileSync(patterns_dir + '/00-test/457-listitem.mustache', 'utf8');
   listPattern.extendedTemplate = listPattern.template;
   listPattern.stylePartials = pattern_assembler.find_pattern_partials_with_style_modifiers(listPattern);
-  
+  listPattern.parameteredPartials = pattern_assembler.find_pattern_partials_with_parameters(listPattern);
+
   var listParentPattern = new Pattern('00-test/457-parent.mustache');
   listParentPattern.template = fs.readFileSync(patterns_dir + '/00-test/457-parent.mustache', 'utf8');
   listParentPattern.extendedTemplate = listParentPattern.template;
   listParentPattern.stylePartials = pattern_assembler.find_pattern_partials_with_style_modifiers(listParentPattern);
-  console.log(listParentPattern)
+  listParentPattern.parameteredPartials = pattern_assembler.find_pattern_partials_with_parameters(listParentPattern);
+
   pl.patterns.push(listPattern);
   pl.patterns.push(listParentPattern);
-  
+
   //act
-  list_item_hunter.process_list_item_partials(listParentPattern, pl);
-  
-  //assert. here we expect {{styleModifier}} to be replaced with an empty string or the styleModifier value from the found partial with the :styleModifier
-  var expectedValue = '<div class="test_group"> <span class="test_base "> Foo </span> <span class="test_base test_1"> Foo </span> <span class="test_base "> Foo </span> <span class="test_base "> Bar </span> <span class="test_base test_1"> Bar </span> <span class="test_base "> Bar </span> </div>';
+  parameter_hunter.find_parameters(listParentPattern, pl);
+
+  //assert.
+  var expectedValue = 'title<ul><li>a</li><li>b</li><li>c</li></ul>';
   test.equals(listParentPattern.extendedTemplate.replace(/\s\s+/g, ' ').replace(/\n/g, ' ').trim(), expectedValue.trim());
   test.end();
 });
-
-
